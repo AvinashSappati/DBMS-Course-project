29442aa15857bd901c7818cf0898be9cec09a491
AvinashSappati <sappatiavinash@gmail.com>
2026-04-24 04:40:37 +0530

    updated app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,44 +1,36 @@
-from fastapi import FastAPI, UploadFile, File, Form
+from fastapi import FastAPI, UploadFile, Form
 import json
-
 from test_model import Text2SQLEngine
-from parser import parse_schema_text_to_json  # your parser
-from fastapi.middleware.cors import CORSMiddleware
+from parser import parse_schema_text_to_json
 
 app = FastAPI()
 
+engine = None
+
+@app.on_event("startup")
+def load_model():
+    global engine
+    print("Loading model...")
+    engine = Text2SQLEngine("dummy.json")
+
 @app.get("/")
 def home():
-    return {"msg": "Text2SQL API running 🚀"}
-
+    return {"msg": "Backend running"}
 
 @app.post("/generate")
-async def generate(file: UploadFile = File(...), question: str = Form(...)):
-    
-    # read schema.txt
-    content = await file.read()
-    raw_text = content.decode("utf-8")
-    
-    # convert to JSON
+async def generate(file: UploadFile, question: str = Form(...)):
+    global engine
+
+    raw_text = await file.read()
+    raw_text = raw_text.decode("utf-8")
+
     schema_json = parse_schema_text_to_json(raw_text)
-    
+
     with open("temp.json", "w") as f:
         json.dump(schema_json, f)
-    
+
     db_id = schema_json[0]["db_id"]
-    
-    # load engine dynamically
-    engine = Text2SQLEngine("temp.json")
-    
+
     result = engine.generate(question, db_id)
-    
+
     return result
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
