0eba0b9eed67713654b483cf52b35005f401f3c0
AvinashSappati <sappatiavinash@gmail.com>
2026-04-24 22:26:37 +0530

    updated API_URL for testing

--- Frontend/script.js
+++ Frontend/script.js
@@ -18,7 +18,7 @@ async function generate() {
 
    try {
     // ⚠️ KEEP YOUR EXISTING LOCALTUNNEL URL HERE
-    const API_URL = "https://heavy-taxis-start.loca.lt/generate"; 
+    const API_URL = "https://eager-bees-flow.loca.lt/generate"; 
 
     const res = await fetch(API_URL, {
       method: "POST",
